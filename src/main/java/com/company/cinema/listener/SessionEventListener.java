b1a71be0de7f25e950db8994c6ce6b54bde2c734
AndreNorb3rt <andrei.ustuzhaninoff@gmail.com>
2022-07-04 21:07:31 +0400

    Edit listener

--- src/main/java/com/company/cinema/listener/SessionEventListener.java
+++ src/main/java/com/company/cinema/listener/SessionEventListener.java
@@ -3,6 +3,7 @@ package com.company.cinema.listener;
 import com.company.cinema.entity.CinemaHall;
 import com.company.cinema.entity.Seat;
 import com.company.cinema.entity.Session;
+import com.company.cinema.entity.Ticket;
 import io.jmix.core.DataManager;
 import io.jmix.core.event.EntitySavingEvent;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class SessionEventListener {
@@ -21,15 +23,17 @@ public class SessionEventListener {
     @EventListener
     public void onSessionSaving(EntitySavingEvent<Session> event) {
         if (event.isNewEntity()){
-            createTickets(event.getEntity().getCinemaHall());
+            createTickets(event.getEntity());
         }
     }
-    private void createTickets(CinemaHall searchCinemaHall){
-        ArrayList<Seat> seatInCinemaHall = dataManager.loadValues("select CinemaHall "+
-                        "from Seat "+
-                        "where Seat.cinemaHall = searchCinemaHall "
-                        )
-
-
+    private void createTickets(Session x){
+        List<Seat> seatInCinemaHall = dataManager.load(Seat.class)
+                .query("select c from Seat c ")
+                .list();//.stream().filter(e -> e.<Seat>getCinemaHall() == x.getCinemaHall());
+        Ticket newTicket = dataManager.create(Ticket.class);
+        newTicket.setSession(x);
+        newTicket.setSeat(seatInCinemaHall.get(0));
+        newTicket.setCost(seatInCinemaHall.get(0).getDefaultCost());
+        dataManager.save(newTicket);
     }
 }
