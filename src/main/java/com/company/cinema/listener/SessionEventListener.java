9909ad86a858ced9ca5ed5ebfebdc2440f2c16c4
AndreNorb3rt <andrei.ustuzhaninoff@gmail.com>
2022-07-05 15:47:11 +0400

    Duration exception

--- src/main/java/com/company/cinema/listener/SessionEventListener.java
+++ src/main/java/com/company/cinema/listener/SessionEventListener.java
@@ -5,11 +5,17 @@ import com.company.cinema.entity.Seat;
 import com.company.cinema.entity.Session;
 import com.company.cinema.entity.Ticket;
 import io.jmix.core.DataManager;
+import io.jmix.core.event.EntityChangedEvent;
+import io.jmix.core.event.EntityLoadingEvent;
 import io.jmix.core.event.EntitySavingEvent;
+import io.jmix.ui.Dialogs;
+import io.jmix.ui.screen.Subscribe;
+import org.codehaus.groovy.control.messages.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import java.awt.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -19,21 +25,22 @@ public class SessionEventListener {
 
     @Autowired
     private DataManager dataManager;
+    private Dialogs dialogs;
 
     @EventListener
-    public void onSessionSaving(EntitySavingEvent<Session> event) {
-        if (event.isNewEntity()){
-            createTickets(event.getEntity());
-        }
+    public void onSessionChangedBeforeCommit(EntityChangedEvent<Session> event) {
+        //if (event.getType() == EntityChangedEvent.Type.DELETED){}
+
     }
-    private void createTickets(Session x){
-        List<Seat> seatInCinemaHall = dataManager.load(Seat.class)
-                .query("select c from Seat c ")
-                .list();//.stream().filter(e -> e.<Seat>getCinemaHall() == x.getCinemaHall());
-        Ticket newTicket = dataManager.create(Ticket.class);
-        newTicket.setSession(x);
-        newTicket.setSeat(seatInCinemaHall.get(0));
-        newTicket.setCost(seatInCinemaHall.get(0).getDefaultCost());
-        dataManager.save(newTicket);
+
+
+
+    @EventListener
+    public void onSessionSaving(EntitySavingEvent<Session> event){
+        //в load evente постоянно создются билеты(
+
+        //if (event.isNewEntity())
+        //    createTickets(event.getEntity());
     }
+
 }
--- src/main/java/com/company/cinema/screen/session/SessionBrowse.java
+++ src/main/java/com/company/cinema/screen/session/SessionBrowse.java
@@ -1,5 +1,6 @@
 package com.company.cinema.screen.session;
 
+import io.jmix.ui.component.Button;
 import io.jmix.ui.screen.*;
 import com.company.cinema.entity.Session;
 
@@ -7,4 +8,8 @@ import com.company.cinema.entity.Session;
 @UiDescriptor("session-browse.xml")
 @LookupComponent("sessionsTable")
 public class SessionBrowse extends StandardLookup<Session> {
+    @Subscribe("infoBtn")
+    public void onInfoBtnClick(Button.ClickEvent event) {
+        
+    }
 }
